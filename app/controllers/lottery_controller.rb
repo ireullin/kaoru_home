028bf8245723eef8c9a602c0965e691b5100fe43
root <root@Develop.(none)>
2014-10-18 17:25:12 +0800

    be able to accept post without token

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -1,5 +1,8 @@
 class LotteryController < ApplicationController
-  	def index
+  	
+    skip_before_action :verify_authenticity_token
+
+    def index
   		if params[:type]=='superlottos'
   			@data = Superlottos.order(term: :desc).page(params[:page]).per(10)
   		elsif params[:type]=='lottery649s'
@@ -19,37 +22,51 @@ class LotteryController < ApplicationController
   		end
 
   		# it's neccessary for phantomjs
-    	headers['Access-Control-Allow-Origin'] = '*'
-    	headers['Access-Control-Allow-Headers'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    	headers['Access-Control-Allow-Methods'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
+    	#headers['Access-Control-Allow-Origin'] = '*'
+    	#headers['Access-Control-Allow-Headers'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    	#headers['Access-Control-Allow-Methods'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
   		
   		respond_to do |format|
-    	    format.json { render :json => @data.to_json  }
-      	end
+    	  format.json { render :json => @data.to_json  }
+      end
 	end
+
 
 	def new
 
 		obj = JSON.parse(params[:data])
 
-		"""
-		if obj[:type]=='superlottos'
-  			#data = JSON.parse( params[:data] )
-  			Superlottos.new(  obj )
-  			Superlottos.save
 
-  		elsif obj[:type]=='lottery649s'
-			Lottery649s.new(  obj )
-  			Lottery649s.save
+    #can't use symbol in obj
+		if obj['type']=='superlottos'
+  		@data = Superlottos.new
+  	elsif obj['type']=='lottery649s'
+    	@data = Lottery649s.new
+   	else
+  		respond_to {|format| format.json { render :json => obj['type'] }}
+      return
+  	end
 
-  		else
-  			
-  		end
-  		"""
+    @data.term = obj['term']
+    @data.no1 = obj['no1']
+    @data.no2 = obj['no2']
+    @data.no3 = obj['no3']
+    @data.no4 = obj['no4']
+    @data.no5 = obj['no5']
+    @data.no6 = obj['no6']
+    @data.special = obj['special']
+    @data.announced_at = obj['announced_at']
 
-		respond_to do |format|
-    	    format.json { render :json => "OK".to_json }
-      	end
+
+    respond_to do |format|
+      if @data.save
+        format.json { render :show, status: :ok, location: @data }
+      else
+        format.json { render json: @data.errors, status: :unprocessable_entity }
+      end
+    end
+
+		
 	end
 
 end
