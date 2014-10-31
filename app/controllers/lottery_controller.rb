5ac4f9a64bc2af47c3c510e65dbfbc3ca0bd7a29
ireullin <cheinheng.lin@uitox.com>
2014-10-31 19:02:25 +0800

    leaving office

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -75,26 +75,40 @@ class LotteryController < ApplicationController
 
         data = Superlottos.all
         
-        all_no = Array.new
-        data.each {|row| all_no.concat([ row['no1'],row['no2'],row['no3'],row['no4'],row['no5'],row['no6'] ]) }
+        normal_no = Array.new
+        special_no = Array.new
+        data.each do |row|
+            normal_no.concat([ row['no1'],row['no2'],row['no3'],row['no4'],row['no5'],row['no6'] ])
+            special_no.push(row['special'])
+        end
 
-        require "rinruby"
-        R.assign "all_no", all_no
-        R.eval """
-            x <- c( all_no )
-            dt <- data.frame(sort(table(x),decreasing = TRUE))
-            cnt = c(dt[,1])
-            num = c(row.names(dt))
-        """
-        rc = {
-            numbers: R.pull("num"),
-            count: R.pull("cnt")
+        normal_no_rc = Statistics.rank_count(normal_no)
+        special_no_rc = Statistics.rank_count(special_no)
+
+
+        max = normal_no_rc['numbers'][0]
+        normal_max = Array.new
+        special_max = Array.new
+        data.each do |row|
+            next unless[ row['no1'],row['no2'],row['no3'],row['no4'],row['no5'],row['no6'] ].include?(max)
+
+            normal_max.concat([ row['no1'],row['no2'],row['no3'],row['no4'],row['no5'],row['no6'] ])
+            special_max.push(row['special'])
+        end
+
+        normal_max_rc = Statistics.rank_count(normal_max)
+        special_max_rc = Statistics.rank_count(special_max)
+
+        total = {
+            normal_no_rc: normal_no_rc,
+            special_no_rc: special_no_rc,
+            normal_max_rc: normal_max_rc,
+            special_max_rc: special_max_rc
         }
 
 
         respond_to do |format|
-            #format.any { render text: render all_no.to_json }
-            format.json { render json: rc.to_json }
+            format.json { render json: total.to_json }
         end
     end
 
