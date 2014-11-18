c3ca3188fdf0b4edf623abae380273ddd164310c
ireullin <cheinheng.lin@uitox.com>
2014-11-18 19:48:29 +0800

    manage of photo albums have complated

--- app/controllers/photo_album_controller.rb
+++ app/controllers/photo_album_controller.rb
@@ -40,7 +40,6 @@ class PhotoAlbumController < ApplicationController
 
 
 	def edit
-		
 		@data = PhotoAlbum.where( path: params[:path] ).first
 
 		respond_to do |format|
@@ -50,23 +49,29 @@ class PhotoAlbumController < ApplicationController
 
 
 	def update
-		@photo_albums = PhotoAlbum.where(path: params[:path]).first
-	    @photo_albums.path = params[:path]
-		@photo_albums.name = params[:name]
-		@photo_albums.yahoo_account = params[:yahoo_account]
-		@photo_albums.api_key = params[:api_key]
-		@photo_albums.shared_secret = params[:shared_secret]
-		@photo_albums.photoset_id = params[:photoset_id]
-		@photo_albums.user_id = params[:user_id]
+		@photo_albums = PhotoAlbum.where(:path => params[:path])
+	    @photo_albums.update_all(
+			name: params[:name],
+			yahoo_account: params[:yahoo_account],
+			api_key: params[:api_key],
+			shared_secret: params[:shared_secret],
+			photoset_id: params[:photoset_id],
+			user_id: params[:user_id],
+			updated_at: Time.now.localtime.strftime("%Y-%m-%d %H:%M:%S")
+		)
 
 	    respond_to do |format|
-	      	if @photo_albums.save
-	        	format.html { redirect_to action: 'manage' }
-	      	else
-		        format.html { render :new }
-		    end
+        	format.html { redirect_to action: 'manage', notice: 'OK' }
 	    end
+	end
 
+
+	def delete
+		PhotoAlbum.where(:path => params[:path]).delete_all
+
+		respond_to do |format|
+        	format.html { redirect_to action: 'manage' }
+	    end
 	end
 
 end
