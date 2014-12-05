88918b480c7787d4f1270fb0c80c336499da8b2b
ireullin <cheinheng.lin@uitox.com>
2014-12-05 17:38:59 +0800

    a kaminari plus which could be changed page by dropdown

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -4,7 +4,7 @@ class LotteryController < ApplicationController
 
     def index
 		if params[:type]=='superlottos'
-			@data = Superlottos.order(term: :desc).page(params[:page]).per(10)
+			@data = Superlottos.order(term: :desc).page(params[:page]).per(10) # page is from kaminari
 		elsif params[:type]=='lottery649s'
 			@data = Lottery649s.order(term: :desc).page(params[:page]).per(10)
 		else
