a8bbb8282d106fa817747c8ac857c77bc8af1b5a
ireullin <cheinheng.lin@uitox.com>
2015-09-07 17:46:58 +0800

    Modified crawler's url to follow source website

--- crawler/download_movies.rb
+++ crawler/download_movies.rb
@@ -8,13 +8,13 @@ $movies = Hash.new
 $schedule = Array.new
 
 def parse_theater(div)
-	
+
 	#p div.css("a")[0].content.gsub(/[\r\n\t ]/,'')
 	data = {
 		theater: div.css("a")[0].content.gsub(/[\r\n\t ]/,''),
 		times: []
 	}
-	
+
 
 	div.css(".col-2").each do |row|
 		times = row.content.gsub(/[\r\n\t]/,'').split(' ')
@@ -27,23 +27,23 @@ end
 
 def get_content(id, name)
 	Net::HTTP.start($URL,80) do |http|
-		
+
 		movie = { movie_id: id, name: name, theaters: [] }
 
-		rsp = http.get("/showtime/showtime.asp?film_id=#{id}&area=a02")
-		a = rsp.body.encode("UTF-8","CP950", :invalid => :replace, :undef => :replace, :replace => "")
+		rsp = http.get("/showtime/#{id}/a02/")
+		#a = rsp.body.encode("UTF-8","CP950", :invalid => :replace, :undef => :replace, :replace => "")
 		#p a
-		html_doc = Nokogiri::HTML(a)
+		html_doc = Nokogiri::HTML(rsp.body)
 		html_doc.css('.showtime01').each do |div|
-			
+
 			theater = parse_theater(div)
 			movie[:theaters].push(theater)
-			
+
 		end
 
 		#p movie
 		$schedule.push movie
-		
+
 	end
 end
 
@@ -61,7 +61,7 @@ def get_movies
 	Net::HTTP.start($URL,80) do |http|
 		rsp = http.get('/home/movie_homepage.html')
 		html_doc = Nokogiri::HTML(rsp.body)
-		
+
 		html_doc.css('select[name=film_id] option').each do |opt|
 			next if opt['value'].strip == ""
 			$movies[ opt['value'] ] = opt.content
@@ -71,7 +71,7 @@ end
 
 
 def main
-		
+
 	begin
 		get_movies
 		get_time
@@ -82,7 +82,7 @@ def main
 		res = Net::HTTP.post_form(uri, { data: $schedule.to_json})
 		puts res.body
 
-	rescue Exception => msg  
+	rescue Exception => msg
 		p msg
 
 	end
