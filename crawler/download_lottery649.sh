e6211790ecb4eb2ae1b2222c1ec7abb682f8232a
ireullin <cheinheng.lin@uitox.com>
2015-10-12 19:39:33 +0800

    Modified for new machine

--- crawler/download_lottery649.sh
+++ crawler/download_lottery649.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-
-/usr/local/bin/phantomjs /var/work/kaoru_home/crawler/download_lottery649.js &> /var/work/kaoru_home/crawler/log/`date +lottery649_%Y%m%d_%H%M%S.log`
-
-
-
-
+/opt/phantomjs/bin/phantomjs /var/work/kaoru_home/crawler/download_lottery649.js &> /var/work/kaoru_home/crawler/log/`date +lottery649_%Y%m%d_%H%M%S.log`
--- crawler/download_superlotto.sh
+++ crawler/download_superlotto.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-
-/usr/local/bin/phantomjs /var/work/kaoru_home/crawler/download_superlotto.js &> /var/work/kaoru_home/crawler/log/`date +superlotto_%Y%m%d_%H%M%S.log`
-
-
-
-
+/opt/phantomjs/bin/phantomjs /var/work/kaoru_home/crawler/download_superlotto.js &> /var/work/kaoru_home/crawler/log/`date +superlotto_%Y%m%d_%H%M%S.log`
